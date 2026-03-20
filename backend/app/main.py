9694dd986966dcb576976c2e4513b6aec1313328
unknown <Mr.Lamzikk@yandex.ru>
2026-03-20 16:45:09 +0300

    start_add_rabbitmq

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,22 +1,47 @@
 from fastapi import FastAPI, UploadFile, HTTPException, File
+from fastapi.responses import JSONResponse
+from fastapi.responses import StreamingResponse, FileResponse
 import pandas as pd
 import os
-from fastapi.responses import JSONResponse
-
+import shutil
 app = FastAPI()
+
+
+@app.get("/files/{filename}")
+async def retrieve_data(filename: str):
+    try:
+        return FileResponse(filename)
+    except Exception as e:
+        HTTPException(status_code=404, detail = "Нету файла")
 
 @app.post("/upload_data")
 async def uploda_data(file: UploadFile = File(...)):
+    file_support = [".xlsx", ".csv", ".xls", ".db"]
+    file_ext = os.path.splitext(file.filename)[1]
+    file_location = os.path.join("./temp_file")
+    if file_ext not in file_support:
+        raise HTTPException(status_code=400, detail="Данный формат не поддерживается")
     try:
-        filename = f"file.json"
-        filepath = os.path.join("./json_data", filename)
-        df = pd.read_excel(file.file)
-        json_data = df.to_json(orient='record', force_ascii = True)
-        with open(filepath, 'w', encoding = 'utf-8') as f:
-            f.write(json_data)
+        with open(file_location, "wb") as f:
+            shutil.copyfileobj(file.file)
+        #task = нужно отправить task в RabbitMq для того чтобы было брокер сообщений 
         return JSONResponse(
-            content=[filename]
+            status_code=2020,
+            content={
+                "task_id": id,
+                "message": "Файл принят в обработку",
+                "status_check_endpoint": f"."#Добавить task_status после 
+            }
         )
     except Exception as e:
-        HTTPException(status_code=500, detail=f"Ошибка работы с файлами{str(e)}")
+        if os.path.exists(file_location):
+            os.remove(file_location)
+        raise HTTPException(status_code=500, detail="Ошибка загрузки")
 
+@app.post("/multiple_files")
+async def upload_data(uploaded_files: list[UploadFile]):
+    for upload_files in uploaded_files:
+        file = upload_files.file
+        filename = upload_files.filename
+        with open(f'1_{filename}', "wb") as f:
+            f.write(file.read())
