a2a6f9eefaa192cfbbd6c40538cd409670b54ca4
unknown <Mr.Lamzikk@yandex.ru>
2026-04-13 11:58:37 +0300

    llm_analyze

--- backend/app/api/endpoint/analyze_json_file.py
+++ backend/app/api/endpoint/analyze_json_file.py
@@ -1,25 +1,27 @@
 from fastapi import APIRouter, HTTPException, Query
 from ...services.llm_service import LLMService_analyze_json
-from ...services.file_processor import FileProcessor
-from pydantic import BaseModel
-from typing import Optional
 import os
 import json
+
 router = APIRouter(prefix = "/analyze_json", tags = ["/analyze"])
 
+BACKEND_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+BASE_DIR = os.path.dirname(BACKEND_DIR)
+json_dir = os.path.join(BASE_DIR, "./json_files")
+
 @router.get("/analyze_json")
-async def analyze_json(json_path: str = Query(...)):
-
+async def analyze_json(filename: str = Query(...)):
+    json_path = os.path.join(json_dir, filename)
     if not os.path.exists(json_path):
-        raise HTTPException(status_code = 404, details = "suck")
-    processor = FileProcessor()
-    with open("json_path", "r", encoding="utf-8") as f:
+              raise HTTPException(status_code=404, detail=f"Файл не найден: {json_path}")
+     
+    with open(json_path, "r", encoding="utf-8") as f:
         data = json.load(f)
-
-    llm_service = LLMService_analyze_json()
-    answer = await llm_service.analyze_json_file(data)
-    return {
-        "json_path": json_path, 
-        "records_count": len(data),
-        "llm_respones": answer
+     
+        llm_service = LLMService_analyze_json()
+        answer = await llm_service.analyze_json_file(data)
+        return {
+              "filename": filename,
+              "records_count": len(data),
+              "llm_response": answer
     }
--- backend/app/services/llm_service.py
+++ backend/app/services/llm_service.py
@@ -2,15 +2,14 @@ from openai import AsyncOpenAI
 from dotenv import load_dotenv
 import os
 import json
-import asyncio #Удалить
 
-load_dotenv()
+load_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), ".env"))
 
 class LLMService_analyze_json:                 
     def __init__(self):
-        api_key = os.getenv("open_api_key")
-        folder_id = os.getenv("yandex_cloud_folder")
-        self.model = os.getenv("yandex_cloud_model", "yandexgpt-lite")
+        api_key = os.getenv("OPEN_API_KEY")
+        folder_id = os.getenv("YANDEX_CLOUD_FOLDER")
+        self.model = os.getenv("YANDEX_CLOUD_MODEL", "yandexgpt-lite")
         self.client = AsyncOpenAI(
             api_key=api_key,  
             base_url="https://llm.api.cloud.yandex.net/v1",
@@ -34,6 +33,24 @@ class LLMService_analyze_json:
         )
         return response.choices[0].message.content
 """
+class ChatingToLLM:
+    def __init__(self):
+        api_key = os.getenv("open_api_key")
+        folder_id = os.getenv("yandex_cloud_folder")
+        self.model = os.getenv("yandex_cloud_model", "yandexgpt-lite")
+        self.client = AsyncOpenAI(
+            api_key=api_key,  
+            base_url="https://llm.api.cloud.yandex.net/v1",
+            default_headers={
+                "x-folder-id": folder_id  
+            }
+        )
+        self.folder_id = folder_id
+    def chatting_to_LLM():
+        return
+
+
+
 async def main():
     test_data = [{"key": "value"}]
     service = LLMService_analyze_json()
